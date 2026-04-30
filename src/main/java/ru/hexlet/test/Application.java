77180ac28e623436c8925e78ee1cd3d61cb94860
artmper <twistertwist1@gmail.com>
2026-04-30 21:30:48 +0300

    create new project

---
+++ src/main/java/ru/hexlet/test/Application.java
@@ -0,0 +1,35 @@
+package ru.hexlet.test;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.Statement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class Application {
+    public static void main(String[] args) throws SQLException {
+        Connection conn = DriverManager.getConnection("jdbc:h2:mem:hexlet-test");
+
+        String sql  = "CREATE TABLE users (id BIGINT PRIMARY KEY AUTO_INCREMENT, username VARCHAR(255), phone BIGINT)";
+        Statement statement = conn.createStatement();
+        statement.execute(sql);
+        statement.close();
+
+        String sql2 = "INSERT INTO users (username, phone) VALUES ('tommy', '123456789'), ('tommy2', '123456789'), ('tommy3', '123456789')";
+        Statement statement2 = conn.createStatement();
+        statement2.executeUpdate(sql2);
+        statement2.close();
+
+        String sql3 = "SELECT * FROM users";
+        Statement statement3 = conn.createStatement();
+        ResultSet resultSet = statement3.executeQuery(sql3);
+
+        while (resultSet.next()) {
+            System.out.println(resultSet.getString("username"));
+            System.out.println(resultSet.getString("phone"));
+        }
+        statement3.close();
+
+        conn.close();
+    }
+}
